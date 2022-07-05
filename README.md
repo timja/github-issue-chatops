79d4fb949ce2ae6ca6cb04f77ea864b1362d2480
Tim Jacomb <timjacomb1+github@gmail.com>
2022-07-05 08:33:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# GitHub issue management - ChatOps
+
+This tool allows managing GitHub issues via ChatOps.
+It uses GitHub webhooks to scale across repositories without needing to add a GitHub action to each of them.
+
+GitHub apps are used for authentication to limit the required permissions.
+
+## Getting started
+
+First you will need to create a GitHub app. Add the permissions required for the commands you are using.
+Add a webhook to the app pointing at your endpoint, the path for the webhook receiver is `/api/github/webhooks`.
+
+### Local development
+
+The easiest way to develop this locally is to use [smee](https://smee.io).
+
+Run `smee --path /api/github/webhooks` and point your webhook to the smee url that is outputted e.g. https://smee.io/Mrl4d3W9AUxeaaa
+
+### Required environment variables
+
+- GITHUB_APP_ID
+- GITHUB_APP_PRIVATE_KEY
+- GITHUB_APP_INSTALLATION_ID
+- SOURCE_OWNER
+- TARGET_OWNER
+- WEBHOOK_SECRET
+
+## Supported commands
+
+### /transfer <destination_repo>
+
+Transfers a GitHub issue to another repository in the same organization.
+
+#### Permissions required
+
+- Contents
+  - Read and write
+- Issues
+  - Read and write
---
+++ index.mjs
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+import { graphql } from '@octokit/graphql'
+import { createServer } from 'http'
+import { Webhooks, createNodeMiddleware } from '@octokit/webhooks';
+import { createAppAuth } from "@octokit/auth-app"
+
+const sourceOwner = process.env.SOURCE_OWNER || 'timja'
+
+const targetOwner = process.env.TARGET_OWNER || 'timja'
+
+const secret = process.env.WEBHOOK_SECRET;
+
+const webhooks = new Webhooks({
+  secret,
+});
+
+const auth = createAppAuth({
+  appId: process.env.GITHUB_APP_ID,
+  privateKey: process.env.GITHUB_APP_PRIVATE_KEY,
+});
+
+webhooks.on('issue_comment.created', ({ id, name, payload }) => {
+  const sourceRepo = payload.repository.name
+  const result = payload.comment.body.match(/\/transfer ([a-zA-Z0-9-]+)/)
+  if (result) {
+    const targetRepo = result[1]
+    console.log(`${id} Transferring issue number ${payload.issue.number} to repo ${targetRepo}`)
+    transferIssue(sourceRepo, targetRepo, payload.issue.number)
+  }
+})
+
+createServer(createNodeMiddleware(webhooks)).listen(3000);
+
+async function transferIssue(sourceRepo, targetRepo, issue) {
+  try {
+    const installationAuthentication = await auth({
+      type: "installation",
+      installationId: process.env.GITHUB_APP_INSTALLATION_ID,
+    });
+
+    const token = installationAuthentication.token
+
+    const { source, target } = await graphql(
+      `
+	query($issue: Int!, $sourceOwner: String!, $sourceRepo: String!, $targetOwner: String!, $targetRepo: String!) {
+		source: repository(owner: $sourceOwner, name: $sourceRepo) {
+			issue(number: $issue) {
+				id
+			}
+		}
+		target: repository(owner: $targetOwner, name: $targetRepo) {
+			id
+		}
+	}
+  `,
+      {
+        sourceOwner,
+        sourceRepo,
+        targetOwner,
+        targetRepo,
+        issue,
+        headers: {
+          authorization: `token ${token}`,
+        },
+      }
+    );
+
+    await graphql(
+      `
+  mutation($issue: ID!, $repo: ID!) {
+		transferIssue(input: {issueId: $issue, repositoryId: $repo}) {
+			issue {
+				url
+			}
+		}
+	}
+  `,
+      {
+        issue: source.issue.id,
+        repo: target.id,
+        headers: {
+          authorization: `token ${token}`,
+        }
+      }
+    )
+  } catch (err) {
+    console.error('Failed to transfer issue', err)
+
+    console.error(JSON.stringify(err.errors))
+  }
+}
+
