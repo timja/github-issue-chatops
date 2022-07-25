00fbd393b5fc49de6abc68d4532257b07b09a256
Joseph Petersen <josephp90@gmail.com>
2022-07-25 11:50:35 +0200

    Add eslint and fix eslint issues (#55)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { createServer } from "http";
+import { createServer } from "node:http";
 import { createNodeMiddleware, Webhooks } from "@octokit/webhooks";
 import { createAppAuth } from "@octokit/auth-app";
 
@@ -8,7 +8,7 @@ import { router } from "./app/router.js";
 const verbose = process.env.VERBOSE === "true";
 
 const secret = process.env.WEBHOOK_SECRET;
-const port = parseInt(process.env.PORT || "3000", 10);
+const port = Number.parseInt(process.env.PORT || "3000", 10);
 
 const webhooks = new Webhooks({
   secret,
@@ -26,10 +26,10 @@ webhooks.on("issue_comment.created", async ({ id, payload }) => {
 createServer(
   createNodeMiddleware(webhooks, {
     // Return 200 for health probes
-    onUnhandledRequest: (request, res) => {
-      res.setHeader("Content-Type", "text/plain");
-      res.write("For webhooks POST to path /api/github/webhooks\n");
-      res.end();
+    onUnhandledRequest: (request, response) => {
+      response.setHeader("Content-Type", "text/plain");
+      response.write("For webhooks POST to path /api/github/webhooks\n");
+      response.end();
     },
   })
 ).listen(port, () => {
