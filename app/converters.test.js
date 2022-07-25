1aff1570e22a9ea55c821cd91aca3bfc2cbb38a2
Joseph Petersen <josephp90@gmail.com>
2022-07-25 10:26:17 +0200

    Use regex split to allow mix of space and comma for reviewers (#48)

--- app/converters.test.js
+++ app/converters.test.js
@@ -43,5 +43,16 @@ describe("converters", () => {
         teams: ["@test-org/team-1", "@test-org/team-2"],
       });
     });
+    test("space and comma separated users and teams", () => {
+      const converted = extractUsersAndTeams(
+        "test-org",
+        "@reviewer1 @test-org/team-1,@reviewer2 @test-org/team-2"
+      );
+
+      expect(converted).toEqual({
+        users: ["@reviewer1", "@reviewer2"],
+        teams: ["@test-org/team-1", "@test-org/team-2"],
+      });
+    });
   });
 });
