ed123bb39d10b2bbc886eed91c043aec5144f788
Tim Jacomb <timjacomb1+github@gmail.com>
2022-07-28 09:08:34 +0100

    One more remove label

--- app/default-config.js
+++ app/default-config.js
@@ -5,7 +5,7 @@ export const defaultConfig = {
       allowed_labels: [],
       enabled: true,
     },
-    "remove-label": {
+    removeLabel: {
       permission: "none",
       allowed_labels: [],
       enabled: true,
