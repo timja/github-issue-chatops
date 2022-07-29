e17986911b0f88dcee64635fd9cbbe65a9dff7dd
Tim Jacomb <timjacomb1+github@gmail.com>
2022-07-29 14:39:36 +0100

    Lint and test

--- app/commands.test.js
+++ app/commands.test.js
@@ -2,18 +2,30 @@ import { getCommands, noneMatch } from "./commands.js";
 
 describe("commands", () => {
   test("noneMatch is true when nothing matches", () => {
-    const commands = getCommands("nothing that would ever match");
+    const commands = getCommands("any", {
+      comment: {
+        body: "nothing that would every match",
+      },
+    });
 
     expect(noneMatch(commands)).toEqual(true);
   });
 
   test("noneMatch is false when something matches", () => {
-    const commands = getCommands("/label one");
+    const commands = getCommands("any", {
+      comment: {
+        body: "/label one",
+      },
+    });
 
     expect(noneMatch(commands)).toEqual(false);
   });
   test("noneMatch is false when multiple matches", () => {
-    const commands = getCommands("/label one /reviewer reviewer");
+    const commands = getCommands("any", {
+      comment: {
+        body: "/label one \n/reviewer reviewer",
+      },
+    });
 
     expect(noneMatch(commands)).toEqual(false);
   });
