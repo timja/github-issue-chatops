5235ae6116c2c05aefe3f8bfd5a45f0aed9f9366
Tim Jacomb <timjacomb1+github@gmail.com>
2022-08-01 20:55:20 +0100

    Handle comments with multiple lines

--- app/matchers.js
+++ app/matchers.js
@@ -1,24 +1,24 @@
 export function transferMatcher(text) {
-  return text.match(/\/transfer ([\dA-Za-z-]+)/);
+  return text.match(/(?:^| | \r\n|\n)\/transfer ([\dA-Za-z-]+)/);
 }
 
 export function closeMatcher(text) {
-  return text.match(/\/close (not-planned)|\/close/);
+  return text.match(/(?:^| | \r\n|\n)\/close (not-planned)|\/close/);
 }
 
 export function reopenMatcher(text) {
-  return text.match(/\/reopen/);
+  return text.match(/(?:^| | \r\n|\n)\/reopen/);
 }
 
 export function labelMatcher(text) {
-  return text.match(/\/label ([\s/A-Za-z\d-,:]+)/);
+  return text.match(/(?:^| | \r\n|\n)\/label ([ /A-Za-z\d-,:]+)/);
 }
 
 export function removeLabelMatcher(text) {
   // TODO prevent matching across lines
-  return text.match(/\/remove-label ([\s/A-Za-z\d-,:]+)/);
+  return text.match(/(?:^| | \r\n|\n)\/remove-label ([ /A-Za-z\d-,:]+)/);
 }
 
 export function reviewerMatcher(text) {
-  return text.match(/\/reviewers? ([\s/@A-Za-z\d-,]+)/);
+  return text.match(/(?:^| | \r\n|\n)\/reviewers? ([ /@A-Za-z\d-,]+)/);
 }
